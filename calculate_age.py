98962232e47c7d635ace91fe92f83346d719a73c
Zaier Vera <veraalber989@gmail.com>
2021-09-22 15:59:36 -0300

    Agregando un programa para saber la edad en años | meses | dias

---
+++ calculate_age.py
@@ -0,0 +1,51 @@
+import time
+from calendar import isleap
+
+
+# Juzgar si el año es biciesto
+def judge_leap_year(year):
+    if isleap(year):
+        return True
+    else:
+        return False
+
+
+# Retorna el numero de dias de cada mes
+def month_days(month, leap_year):
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2 and leap_year:
+        return 29
+    elif month == 2 and (not leap_year):
+        return 28
+
+
+name = input("Ingresa tu Nombre: ")
+age = int(input("Ingresa tu edad: "))
+localtime = time.localtime(time.time())
+
+year = int(age)
+month = year * 12 + localtime.tm_mon
+day = 0
+
+begin_year = int(localtime.tm_year) - year
+end_year = begin_year + year
+
+# Calcular los dias
+for y in range(begin_year, end_year):
+    if (judge_leap_year(y)):
+        day = day + 366
+    else:
+        day = day + 365
+
+
+leap_year = judge_leap_year(localtime.tm_year)
+for m in range(1, localtime.tm_mon):
+    day = day + month_days(m, leap_year)
+
+
+day = day + localtime.tm_mday
+print("%s's age is %d years or " % (name, year), end="")
+print("%d months or %d days" % (month, day))
